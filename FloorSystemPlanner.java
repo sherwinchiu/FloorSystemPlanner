ebf5c1735f05a866d133b0881a8cfe3177a7b84d
Yoson Chiu <yoson.chiu@hotmail.com>
2020-02-13 11:30:02 -0500

    Added FloorSystemPLanner

---
+++ FloorSystemPlanner.java
@@ -0,0 +1,12 @@
+/**  FloorSystemPlanner class 
+  *  Sherwin Chiu and Kyro Nassif
+  *  Visual Display of floor for Prom   
+  *  2/13/2020
+  */
+
+class FloorSystemPlanner{
+    private int dog = 0;
+    public FloorSystemPlanner(){
+
+    }
+}
