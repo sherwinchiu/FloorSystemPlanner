dbc9f67aa90485993c18624b0967a991c0d5eb78
Yoson Chiu <yoson.chiu@hotmail.com>
2020-02-18 12:34:12 -0500

    Finish table class

--- Table.java
+++ Table.java
@@ -1,4 +1,3 @@
-import javax.swing.*;
 import java.awt.*;
 
 class Table{
@@ -6,44 +5,52 @@ class Table{
     private Student[] students;
     private int x;
     private int y;
-    public Table(int x, int y){
-    private int radius;
+    private final int RADIUS = 25;
     
-    public Table(size){
+    public Table(int x, int y, int size){
         this.x = x;
         this.y = y;
-        this.radius = radius;
+        this.size = size;
     }
-    
-    public void draw(Graphics g){
-      g.drawOval(this.x, this.y, this.radius, this.radius);
-    }
-    
     public int getSize(){
         return this.size;
     }
-    
-    public void setSize(int size){
-        this.size = size;
-        students = new Student[size];
-    }
-    
     public void addStudent(Student s){
         for (int i = 0; i < this.size; i++){
-            if (students[i] == null);
-            students[i] = s;
-            i = this.size; //lol idk if he lets us do this
+            if (this.students[i] == null){
+                this.students[i] = s;
+                i = this.size; //lol idk if he lets us do this
+            }
         }
+    }
+    public void removeStudent(Student s){
+        for(int i = 0; i < this.size; i++){
+            if (s.getId() == students[i].getId())
+                this.students[i] = null;
+        }
+    } 
+    public Student[] getStudents(){
+        return this.students;
+    }
+    public void setStudents(ArrayList<Student> s){
+        for(int i = 0; i < this.size; i++)
+            this.students[i] = s;
     }
     public int getX(){
         return this.x;
     }
-    public Student[] getStudents(){
-        return this.students;
+    public void setX(int x){
+        this.x = x;
     }
     public int getY(){
         return this.y;
     }
+    public void setY(int y){
+        this.y = y;
+    }
+
+
+    
     public boolean isFull(){
         if (students[size-1] != null){
             return true;
@@ -51,5 +58,8 @@ class Table{
             return false;
         }
     }
+    public void drawTable(Graphics g){
+        g.drawOval(this.x, this.y, this.RADIUS, this.RADIUS);
+      }
 }
     
